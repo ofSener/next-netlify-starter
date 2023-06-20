c1f072dd6895f16dec5f15816c3eea1ecdf74658
ofSener <92453853+ofSener@users.noreply.github.com>
2023-06-20 21:07:28 +0300

    20 haz

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,65 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import React from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
 
-export default function Home() {
+import LearningPath from './components/LearningPath';
+import TechnicalJourney from './components/TechnicalJourney';
+import whoami from './components/whoami'
+
+const IndexPage = () => {
+  const handleLinkedInClick = () => {
+    // LinkedIn sayfasına yönlendirme işlemleri burada gerçekleştirilebilir
+    window.location.href = 'https://www.linkedin.com/in/omerfaruksener/';
+  };
+
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>My Website</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <h1 className="centered">Ömer Faruk Şener</h1>
+
+        <LearningPath />
+        <hr />
+
+        <h2 className="category">Who Am I</h2>
+        <whoami />
+        <hr />
+
+        <h2 className="category">Technical Journey</h2>
+        <TechnicalJourney />
+        <hr />
+
+        <div className="linkedin-icon" onClick={handleLinkedInClick}>
+          <Image src="/linkedin-icon.png" alt="LinkedIn" width={40} height={40} />
+        </div>
       </main>
 
-      <Footer />
+      <style jsx>{`
+        .container {
+          max-width: 800px;
+          margin: 0 auto;
+          padding: 40px;
+        }
+        .centered {
+          text-align: center;
+        }
+        .category {
+          text-align: center;
+          margin-top: 40px;
+        }
+        .linkedin-icon {
+          display: flex;
+          justify-content: center;
+          margin-top: 40px;
+          cursor: pointer;
+        }
+      `}</style>
     </div>
-  )
-}
+  );
+};
+
+export default IndexPage;
